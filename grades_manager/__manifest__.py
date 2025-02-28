767a12eb0349da42ec0c7ecbddffc2f0ef82ec03
merzamac <mmerza@uc.edu.ve>
2025-02-27 22:56:22 -0400

    nuevo curso

---
+++ grades_manager/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Grades Manager',
+    'summary': 'Handles grades among students and courses',
+    'description': 'Handles grades among students and courses',
+    'author': 'Moises Merza',
+    'category': 'Base',
+    'version': '17.0.0.1',
+    'depends': ['base'],
+    'data': [],
+    'license': 'AGPL-3',
+    'application':True,
+    'installable': True,
+    'auto_install': False,
+}
