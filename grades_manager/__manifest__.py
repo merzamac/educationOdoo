7e506959ed3a90dfcb82e5b42d3ee9f6f544d4bd
merzamac <mmerza@uc.edu.ve>
2025-03-20 18:19:34 -0400

    contact

--- grades_manager/__manifest__.py
+++ grades_manager/__manifest__.py
@@ -9,9 +9,9 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
-        'views/grades_manager_menus.xml',
         'views/grades_course_views.xml',
         'views/res_partner_views.xml',
+        'views/grades_manager_menus.xml',
     ],
     'license': 'AGPL-3',
     'application':True,
